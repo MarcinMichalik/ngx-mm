b7fd0b8ad18c7e83bbd6f6d705468d960b792ffe
MarcinMichalik <marcinmat16@gmail.com>
2024-05-10 22:09:40 +0200

    publish npm package

--- projects/ngx-mm/forms/public-api.ts
+++ projects/ngx-mm/forms/public-api.ts
@@ -3,6 +3,8 @@ export * from './mm-form-field';
 export * from './directives/mm-form-control.directive';
 export * from './directives/mm-form-label.directive';
 export * from './directives/mm-form-helper.directive';
+export * from './directives/mm-form-mandatory.directive';
+export * from './directives/mm-form-error.directive';
 
 export * from './services/mm-error-message-resolver.service'
 
